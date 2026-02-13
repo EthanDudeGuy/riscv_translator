4d1b0f488386b8ef691a672213b63885e0237437
Ethan Emery <ethan.emery01@gmail.com>
2026-02-13 15:12:19 -0800

    added some doc

--- riscv_translator/generate_c.cpp
+++ riscv_translator/generate_c.cpp
@@ -8,9 +8,9 @@
 #include <vector>
 #include <fstream>
 #include <fcntl.h>    
-#include <unistd.h>   // For read() and close()
+#include <unistd.h>
 
-
+//print the C header for the stuff we need to run
 void print_header() {
         printf("#include <stdint.h>\n");
         printf("#include <stdio.h>\n");
@@ -26,7 +26,7 @@ void print_header() {
 	printf("uint64_t starting_address = 0;\n\n");
 }
 
-
+//sets up the memory space and reads in the PTLOAD sections of the ELF
 void print_init_memory() {
 	printf("void init_memory(const char* elf_path) {\n");
 	printf("    // 1. Reserve 4GB virtual address space\n");
@@ -76,7 +76,7 @@ void print_init_memory() {
 	printf("}\n\n");
 	}
 
-
+//prints main
 void print_main() {
         printf("\nint main(int argc, char** argv) {\n");
         printf("    if (argc < 2) { printf(\"Usage: %%s <original_elf>\\n\", argv[0]); return 1; }\n");
@@ -93,7 +93,8 @@ void translate_to_c(csh handle, cs_insn *insn, const std::set<uint64_t>& targets
     // Check the count
     int is_target = targets.count(insn->address);
     
-    // If it's a target, we should actually print the label here!
+    // If it's a target, print the label
+    // NOTE: Need to add labels for the instruction immediately after a call or jalr 
     if (is_target) {
         printf("L_0x%lx:\n", insn->address);
     }
@@ -102,12 +103,14 @@ void translate_to_c(csh handle, cs_insn *insn, const std::set<uint64_t>& targets
 }
 
 void print_run_cpu(csh handle, const uint8_t *code_ptr, size_t code_size, uint64_t address, cs_insn *insn, std::set<uint64_t> targets) {
+	//print the header of the instruction
 	printf("void run_cpu(uint64_t entry_point) {\n");
 	printf("    printf(\"%%lx\\n\", entry_point);\n");
 	while (code_size > 0) {
 		bool success = cs_disasm_iter(handle, &code_ptr, &code_size, &address, insn);
 
 		if (success) {
+			//print the functionally equivalent C for a given instruction
 			translate_to_c(handle, insn, targets);
 		} else {
 			if (code_size >= 4) {
@@ -121,7 +124,7 @@ void print_run_cpu(csh handle, const uint8_t *code_ptr, size_t code_size, uint64
 				address += 4;
 
 			} else {
-				break; //trailing jumk
+				break; //trailing whatever
 			}
 		}
 	
@@ -129,10 +132,12 @@ void print_run_cpu(csh handle, const uint8_t *code_ptr, size_t code_size, uint64
 	printf("}\n");
 }
 
+
 bool is_branch(cs_insn *insn) {
 	cs_detail *detail = insn->detail;
 	if (!detail) return false;
 
+	//find the group labels that indicate a branching RISCV instruction
 	for (int i = 0; i < detail->groups_count; i++) {
 		// Look for the "Jump" or "Branch Relative" categories
 		if (detail->groups[i] == RISCV_GRP_JUMP || 
@@ -148,11 +153,13 @@ bool is_branch(cs_insn *insn) {
 uint64_t get_branch_target(cs_insn *insn) {
     cs_riscv *riscv = &insn->detail->riscv;
 
+    //is this a real instruction
     if (riscv->op_count > 0) {
         cs_riscv_op *last_op = &riscv->operands[riscv->op_count - 1];
 
+	//if we have an immediate branch target
         if (last_op->type == RISCV_OP_IMM) {
-            // FIX: Add the instruction address to the relative offset
+            //Add the instruction address to the relative offset
             return (uint64_t)(insn->address + last_op->imm);
         }
     }
@@ -165,10 +172,12 @@ std::set<uint64_t> collect_branch_targets(csh handle, const uint8_t *code_ptr, s
 	//collect all branchh targets and return the set
 	std::set<uint64_t> targets;
 
+	//loop over the instructions
 	while (code_size > 0) {
 		uint64_t current_insn_address = address;
 		bool success = cs_disasm_iter(handle, &code_ptr, &code_size, &address, insn);
-
+		
+		//dissassembled successfully
 		if (success) {
 			//check if it is a branch and if it is and the immediate exists add to targets
 			if (is_branch(insn)) {
@@ -179,6 +188,7 @@ std::set<uint64_t> collect_branch_targets(csh handle, const uint8_t *code_ptr, s
 				}	
 			}
 
+		//failed, likely custom instruction, increase pointers
 		} else {
 			code_ptr += 4;
 			code_size -= 4;
@@ -250,10 +260,7 @@ int main(int argc, char** argv) {
 
 	close(fd);
 
-        //dissasemble binary
-	//const uint8_t *code_ptr = buffer.data();
-        //size_t code_size = buffer.size();
-        //uint64_t address = 0x11158; //starting address????
+	//OUTPUT THE FUNCTIONALLY EQUIVALENT C
         cs_insn *insn = cs_malloc(handle);
 	uint64_t elf_entry = ehdr.e_entry;
 	
@@ -274,20 +281,5 @@ int main(int argc, char** argv) {
         cs_free(insn, 1);
         cs_close(&handle);
         return 0;
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
